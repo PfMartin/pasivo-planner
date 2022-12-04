e00afd3550b3f5a56480d207a66d3b20f01d06b3
Martin <martinpfatrisch@gmail.com>
2022-12-04 16:46:59 +0100

    Simplified the animation a lot

--- app/src/components/ProgressBar.vue
+++ app/src/components/ProgressBar.vue
@@ -1,33 +1,19 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { computed } from 'vue';
 
 const props = defineProps<{
   progress: number;
 }>();
 
-const progressMap: Record<string, string> = {
-  ['0']: 'empty',
-  ['20']: 'twenty',
-  ['40']: 'fourty',
-  ['60']: 'sixty',
-  ['80']: 'eigthy',
-  ['100']: 'full',
-};
-
-const barClass = ref(['progress', 'zero']);
-watch(
-  () => props.progress,
-  (newValue: number) => {
-    const progressString = `${newValue}`;
-    barClass.value = ['progress', progressMap[progressString]];
-  }
-);
+const width = computed(() => {
+  return `${props.progress}%`;
+});
 </script>
 
 <template>
   <div class="progress-bar">
     <div class="bar">
-      <div :class="barClass"></div>
+      <div class="progress" :style="`width: ${width}`"></div>
     </div>
   </div>
 </template>
@@ -51,24 +37,5 @@ watch(
   height: 100%;
   background: #68937c;
   transition: width 0.6s ease-out;
-}
-.progress.empty {
-  width: 0%;
-}
-.progress.twenty {
-  width: 20%;
-}
-.progress.fourty {
-  width: 40%;
-}
-.progress.sixty {
-  width: 60%;
-}
-.progress.eighty {
-  width: 80%;
-}
-
-.progress.full {
-  width: 100%;
 }
 </style>
