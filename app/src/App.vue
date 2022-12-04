6022f66cd5c69b14c261882448cbbc5fc55b6f10
Martin <martinpfatrisch@gmail.com>
2022-12-04 14:40:32 +0100

    Added vue-router and alias for src

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,25 +1,9 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
-import LandingPage from "./components/LandingPage.vue";
-import LifeSituation from "./components/LifeSituation.vue";
-
-const route = ref("landingPage");
-
-const isLandingPage = computed(() => route.value === "landingPage");
-const isLifeSituation = computed(() => route.value === "lifeSituation");
-
-const routeForward = (): void => {
-  route.value = "lifeSituation";
-  console.log(isLifeSituation.value);
-};
-const routeBackward = (): void => {
-  route.value = "landingPage";
-};
+import Home from "@/views/Home.vue";
 </script>
 
 <template>
-  <LandingPage v-if="isLandingPage" @forward-button-clicked="routeForward" />
-  <LifeSituation v-if="isLifeSituation" @back-button-clicked="routeBackward" />
+  <Home />
 </template>
 
 <style scoped></style>
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import "@/style.css";
+import App from "@/App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
 
-createApp(App).mount("#finance-planner-app");
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{ path: "/", name: "Home", component: Home }],
+});
+
+createApp(App).use(router).mount("#finance-planner-app");
