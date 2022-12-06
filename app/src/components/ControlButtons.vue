11d8327f5535f2bd7f8b3aca2dd2f41181121fe0
Martin <martinpfatrisch@gmail.com>
2022-12-06 18:03:06 +0100

    Added positions of control-buttons

--- app/src/components/ControlButtons.vue
+++ app/src/components/ControlButtons.vue
@@ -7,9 +7,43 @@ defineProps<{
 
 <template>
   <div class="control-buttons">
-    <RouterLink v-if="toBack" :to="{ name: toBack }">Back</RouterLink>
-    <RouterLink v-if="toNext" :to="{ name: toNext }">Next</RouterLink>
+    <RouterLink class="back-button" v-if="toBack" :to="{ name: toBack }"
+      >Back</RouterLink
+    >
+    <RouterLink class="next-button" v-if="toNext" :to="{ name: toNext }"
+      >Next</RouterLink
+    >
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.control-buttons {
+  position: relative;
+
+  a {
+    background: #cee4c3;
+    // background: #68937c;
+    color: #091301;
+    text-decoration: none;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid #091301;
+    border-radius: 10px;
+    transition: background 0.2s;
+
+    &.back-button {
+      position: absolute;
+      left: 0;
+    }
+
+    &.next-button {
+      position: absolute;
+      right: 0;
+    }
+
+    &:hover {
+      // background: #58836c;
+      background: #bed4b3;
+    }
+  }
+}
+</style>
